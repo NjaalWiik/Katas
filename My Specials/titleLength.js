7bf66ad829b31bdc2b0716e5caef146609df82e7
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-03 09:51:38 +0200

    New katas

---
+++ My Specials/titleLength.js
@@ -0,0 +1,6 @@
+const testString =
+  'Coffee Drinker&#39;s Delight: Handy and Elegant All-Wood Combination Coffee Scoop and Bag Closer';
+
+const titleLength = title => title.length;
+
+titleLength(testString);
