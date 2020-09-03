caf3ca94a565a8566d204969e33c1e258a8c43da
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-03 13:07:29 +0200

    photosNbr

---
+++ My Specials/Photos/Number of photos/photosNbr.js
@@ -0,0 +1,48 @@
+const testData = [
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/c/1639/1303/707/847/il/3e3bc0/2315943457/il_170x135.2315943457_6uiu.jpg',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/846d66/2008051164/il_170x135.2008051164_7dgm.jpg?version=0',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/e4e21f/2268345650/il_170x135.2268345650_nogr.jpg?version=0',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/5c872c/2055639899/il_170x135.2055639899_nv9p.jpg?version=0',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/13bd1e/2268345718/il_170x135.2268345718_izet.jpg?version=0',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/934de4/1302430296/il_170x135.1302430296_geeh.jpg?version=0',
+    full_height: 3000,
+    full_width: 3000
+  },
+  {
+    url_170x135:
+      'https://i.etsystatic.com/10204022/d/il/3fb3f6/2008050934/il_170x135.2008050934_ddvh.jpg?version=0',
+    full_height: 2000,
+    full_width: 2000
+  }
+];
+
+const photosNbr = array => array.length;
+
+photosNbr(testData);
