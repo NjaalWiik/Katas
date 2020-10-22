04f82d38513b113ccee630dffeffd35662944084
Njaal Wiik <njaal.wiik@gmail.com>
2020-10-22 14:42:07 +0200

    Update threeNumberSum.js

--- Arrays/Medium/threeNumberSum.js
+++ Arrays/Medium/threeNumberSum.js
@@ -2,9 +2,9 @@
 // function should find all triplets in the array that sum up to the target sum and return a two-dimensional array of all
 // these triplets.
 
-// The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.
+// * The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.
 
-// If no three numbers sum up to the target sum, the function should return an empty array.
+// * If no three numbers sum up to the target sum, the function should return an empty array.
 
 const testArray = [12, 3, 1, 2, -6, 5, -8, 6];
 const testTargetSum = 0;
