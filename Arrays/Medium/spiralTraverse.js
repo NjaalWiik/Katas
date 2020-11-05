e8d239c10e5cdbaf6c0d7a2b44fa2bd8345b2e83
Njaal Wiik <njaal.wiik@gmail.com>
2020-11-05 09:11:33 +0100

    Done

--- Arrays/Medium/spiralTraverse.js
+++ Arrays/Medium/spiralTraverse.js
@@ -3,7 +3,7 @@ function spiralTraverse(array) {
 
   let startCol = 0;
   let endCol = array[0].length - 1;
-  let startRow = 1;
+  let startRow = 0;
   let endRow = array.length - 1;
 
   while (startCol <= endCol && startRow <= endRow) {
@@ -11,15 +11,21 @@ function spiralTraverse(array) {
       result.push(array[startCol][i]);
     }
 
-    for (let i = startRow; i <= endRow; i++) {
+    for (let i = startRow + 1; i <= endRow; i++) {
       result.push(array[i][endCol]);
     }
 
     for (let i = endCol - 1; i >= startCol; i--) {
+      if (startRow === endRow) {
+        break;
+      }
       result.push(array[endRow][i]);
     }
 
-    for (let i = endRow - 1; i >= startRow; i--) {
+    for (let i = endRow - 1; i >= startRow + 1; i--) {
+      if (startCol === endCol) {
+        break;
+      }
       result.push(array[i][startCol]);
     }
 
